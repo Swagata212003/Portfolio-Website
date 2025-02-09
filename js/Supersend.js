e1240f9635408624805ac8c670f5239a1ee7d2c0
Swagata@212003 <Swagataroy594@gmail.com>
2025-02-09 20:53:43 +0530

    updated

---
+++ js/Supersend.js
@@ -0,0 +1,10 @@
+import SuprSendInbox from '@suprsend/react-inbox'
+import 'react-toastify/dist/ReactToastify.css' // needed for toast notifications, can be ignored if hideToast=true
+
+// add to your react component
+<SuprSendInbox
+  workspaceKey= "KYUfO0xrYO8K8pcgc4UG"
+  subscriberId= "prj_NyR82LWBQSkL7Jlma63J2faSVuf0"
+  distinctId= "royswagata"
+/>
+
